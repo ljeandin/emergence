9e79433e7a678838889a4741d29a8d10ddca9bbe
ljeandin <lucie.jeandin@gmail.com>
2025-03-12 08:25:54 +0100

    Create Hero

---
+++ src/components/organisms/hero/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="hero">
+        <div class="hero__text">
+            <h1 class="heading-3">Emergence Feng-Shui</h1>
+            <h2 class="heading-1">
+                But I must explain to you how all this
+                mistaken idea of denouncing pleasure and
+                praising pain was born and I will give you a
+                complete account of the system.
+            </h2>
+            <button class="button button--light">
+                Prenons rendez-vous
+            </button>
+        </div>
+        <div class="hero__image-wrapper">
+            <img
+                class="hero__image"
+                src="/src/assets/images/hero/hero_image.jpg"
+                alt=""
+            />
+        </div>
+    </section>
+</template>
