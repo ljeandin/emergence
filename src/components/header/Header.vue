5fada328a6f4a41a146aa3d09dff23ece32689af
ljeandin <lucie.jeandin@gmail.com>
2025-03-12 07:43:33 +0100

    Create header

---
+++ src/components/header/Header.vue
@@ -0,0 +1,42 @@
+<script setup>
+    const navLinks = [
+        {
+            name: "Accueil",
+            link: "#",
+        },
+        {
+            name: "Le feng-shui",
+            link: "#",
+        },
+        {
+            name: "Témoignages",
+            link: "#",
+        },
+        {
+            name: "Services",
+            link: "#",
+        },
+        {
+            name: "À propos",
+            link: "#",
+        },
+        {
+            name: "Réservation",
+            link: "#",
+        },
+    ];
+</script>
+
+<template>
+    <header>
+        <nav class="header">
+            <a
+                class="header__link"
+                v-for="link in navLinks"
+                :href="link.link"
+            >
+                {{ link.name }}
+            </a>
+        </nav>
+    </header>
+</template>
