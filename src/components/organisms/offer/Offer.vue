a1e1a38209873328fefe5ce9436d180bbc2dcb70
ljeandin <lucie.jeandin@gmail.com>
2025-03-13 07:14:10 +0100

    Beautify Offers

--- src/components/organisms/offer/Offer.vue
+++ src/components/organisms/offer/Offer.vue
@@ -1,33 +1,41 @@
 <script setup>
-    const offer_part = [
+    const offers = [
         {
-            picture: new URL(
-                "@/assets/images/offer/offer_part_1.jpg",
-                import.meta.url
-            ).href,
-            title: "1er pilier : Le rapport d’expertise",
-            content:
-                "Une analyse complète de ton habitat selon les principes du Feng Shui traditionnel (l'école de la forme, sectorisation Bagua et carré LoShu, manques/extensions, les 8 palais des habitants, leurs orientations favorables, les remèdes et activateurs liés aux étoiles volantes), guidée par des vidéos explicatives et un calendrier “plan d’action”.",
+            title: "1er pilier",
+            subtitle: "Le rapport d’expertise",
+            type: "paragraph",
+            content: `Une analyse complète de ton habitat selon les principes du Feng Shui traditionnel (l'école de la forme,
+              sectorisation Bagua et carré LoShu, manques/extensions, les 8 palais des habitants, leurs orientations favorables,
+              les remèdes et activateurs liés aux étoiles volantes), <b>guidée par des vidéos explicatives</b> et un
+              <b>calendrier “plan d’action”</b>.`,
         },
         {
-            picture: new URL(
-                "@/assets/images/offer/offer_part_2.jpg",
-                import.meta.url
-            ).href,
-            title: "2 ème pilier : L’accompagnement",
-            content:
-                "- 1 Atelier d’intention Feng Shui par mois posez vos intentions, partagez vos expériences et renforcez votre connexion à votre environnement. - 1 Séance de questions/réponses par semaine :  affine ta compréhension et co-construire des solutions décoratives adaptées à ton espace. - 1 Communauté bienveillante et inspirante où tu pourras partager ton parcours, poser tes questions et célébrer tes avancées.",
+            title: "2ème pilier",
+            subtitle: "L’accompagnement",
+            type: "list",
+            content: [
+                `<b>1 Atelier d’intention Feng Shui par mois</b> posez vos intentions, partagez vos expériences et renforcez votre connexion à votre environnement.`,
+                `<b>1 Séance de questions/réponses par semaine :</b> affine ta compréhension et co-construire des solutions décoratives adaptées à ton espace.`,
+                `<b>1 Communauté bienveillante et inspirante</b> où tu pourras partager ton parcours, poser tes questions et célébrer tes avancées.`,
+            ],
         },
         {
-            picture: new URL(
-                "@/assets/images/offer/offer_part_3.jpg",
-                import.meta.url
-            ).href,
-            title: "3 ème pilier : L’ancrage",
-            content:
-                "3 Méditations guidées : elles te guideront au fur et à mesure de l’accompagnement pour manifester la transformation que tu mérites. Découvertes symboliques, purification et lithothérapie : utilise des objets porteurs de sens et intègre la lithothérapie dans les pièces principales. Il est essentiel de rafraichir l’énergie qui a stagné et de se débarrasser des énergies négatives.",
+            title: "3ème pilier",
+            subtitle: "L’ancrage",
+            type: "list",
+            content: [
+                `<b>3 Méditations guidées :</b> elles te guideront au fur et à mesure de l’accompagnement pour manifester la transformation que tu mérites.`,
+                `<b>Découvertes symboliques, purification et lithothérapie:</b> utilise des objets porteurs de sens et intègre la lithothérapie dans les pièces principales. Il est essentiel de rafraichir l’énergie qui a stagné et de se débarrasser des énergies négatives.`,
+            ],
         },
     ];
+
+    const part_image_URL_maker = function (imageNumber) {
+        return new URL(
+            `../../../assets/images/offer/offer_part_${imageNumber}.jpg`,
+            import.meta.url
+        ).href;
+    };
 </script>
 
 <template>
@@ -47,25 +55,37 @@
             <a
                 class="button-discrete"
                 href=""
-                >Découvre les 3 piliers essentiels à ton
-                épanouissement</a
             >
+                Découvre les 3 piliers essentiels à ton
+                épanouissement
+            </a>
         </div>
+
         <div
-            v-for="(part, index) in offer_part"
+            v-for="(offer, index) in offers"
             :key="index"
             class="offer-part"
         >
             <img
                 class="offer-part__image"
-                :src="part.picture"
+                :src="part_image_URL_maker(index + 1)"
                 alt=""
             />
             <div class="offer-part__text">
                 <h2 class="offer-part__title heading-2">
-                    {{ part.title }}
+                    {{ offer.title }} :
+                    <em>{{ offer.subtitle }}</em>
                 </h2>
-                <p>{{ part.content }}</p>
+                <div v-if="offer.type === 'paragraph'">
+                    <p v-html="offer.content"></p>
+                </div>
+                <ul v-else>
+                    <li
+                        v-for="(item, idx) in offer.content"
+                        :key="idx"
+                        v-html="item"
+                    ></li>
+                </ul>
             </div>
         </div>
     </section>
